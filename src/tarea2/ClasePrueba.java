fa323c5beb7d228af872228d6682845185ccaeb6
Damian Felix <damianfelix22@gmail.com>
2023-08-26 20:11:21 -0300

    subida de clase de prueba

---
+++ src/tarea2/ClasePrueba.java
@@ -0,0 +1,13 @@
+public class Prueba {
+ 
+    private int propiedad1;
+    
+    public void setPropiedad(int propiedad) {
+        this.propiedad1 = propiedad;
+    }
+
+    public int getPropiedad() {
+        return this.propiedad1;
+    }
+    
+}
